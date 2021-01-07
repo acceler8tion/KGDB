150d080872d9da72fbf2ca52aeff2c9ec2b9c685
acceler8tion <rurchi1206@gmail.com>
2021-01-08 03:01:31 +0900

    enhancement: fully added JSDoc

--- kakaoGDBot-api/demonlist.js
+++ kakaoGDBot-api/demonlist.js
@@ -2,18 +2,20 @@
 /**
  * DemonList full form 데몬 pojo
  * 
- * @param {Number} id 
- * @param {String} name 
- * @param {Number} position 
- * @param {Number} requirement 
- * @param {Array<DemonListPlayer>} creators 
- * @param {Array<DemonListRecord>} records 
- * @param {String} video 
+ * @param {Number} id 데몬리스트 전용 데몬 ID
+ * @param {String} name 데몬 이름
+ * @param {Number} position 데몬리스트 순위
+ * @param {Number} levelId 레벨 ID
+ * @param {Number} requirement 최소한으로 요구되는 기록의 퍼센트
+ * @param {Array<DemonListPlayer>} creators 데몬 제작자들
+ * @param {Array<DemonListRecord>} records 데몬 기록들
+ * @param {String} video 데몬 비디오 링크
  */
-function DemonListData(id, name, position, requirement, creators, records, video) {
+function DemonListData(id, name, position, levelId, requirement, creators, records, video) {
     this.id = id;
     this.name = name;
     this.position = position;
+    this.levelId = levelId;
     this.requirement = requirement;
     this.creators = creators;
     this.records = records;
@@ -25,11 +27,11 @@ function DemonListData(id, name, position, requirement, creators, records, video
 /**
  * DemonList listed form 레코드 pojo
  * 
- * @param {Number} id 
- * @param {DemonListPlayer} player 
- * @param {Number} progress 
- * @param {String} status 
- * @param {String} video 
+ * @param {Number} id 데몬리스트 전용 기록 ID
+ * @param {DemonListPlayer} player 기록 등록자
+ * @param {Number} progress 기록의 퍼센트
+ * @param {String} status 데몬리스트에서 설정한 기록의 상태
+ * @param {String} video 기록 비디오 링크
  */
 function DemonListRecord(id, player, progress, status, video) {
     this.id = id;
@@ -44,9 +46,9 @@ function DemonListRecord(id, player, progress, status, video) {
 /**
  * DemonList minimal form 플레이어 pojo
  * 
- * @param {Number} id 
- * @param {String} name 
- * @param {Boolean} banned 
+ * @param {Number} id 데몬리스트 전용 플레이어 ID
+ * @param {String} name 플레이어 이름(Do not sync from in game username)
+ * @param {Boolean} banned 플레이어의 밴 여부
  */
 function DemonListPlayer(id, name, banned) {
     this.id = id;

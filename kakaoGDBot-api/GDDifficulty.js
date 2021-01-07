2c5894d0704e096d2c824f894a28c71634c1b8b6
acceler8tion <rurchi1206@gmail.com>
2021-01-07 17:44:22 +0900

    value type changed

--- kakaoGDBot-api/GDDifficulty.js
+++ kakaoGDBot-api/GDDifficulty.js
@@ -1,16 +1,16 @@
 module.exports = {
-    NA: 0,
-    AUTO: 1,
-    EASY: 2,
-    NORMAL: 3,
-    HARD: 4,
-    HARDER: 5,
-    INSANE: 6,
-    EASY_DEMON: 7,
-    MEDIUM_DEMON: 8,
-    HARD_DEMON: 9,
-    INSANE_DEMON: 10,
-    EXTREME_DEMON: 11,
+    NA: "NA",
+    AUTO: "AUTO",
+    EASY: "EASY",
+    NORMAL: "NORMAL",
+    HARD: "HARD",
+    HARDER: "HARDER",
+    INSANE: "INSANE",
+    EASY_DEMON: "EASY_DEMON",
+    MEDIUM_DEMON: "MEDIUM_DEMON",
+    HARD_DEMON: "HARD_DEMON",
+    INSANE_DEMON: "INSANE_DEMON",
+    EXTREME_DEMON: "EXTREME_DEMON",
 
     /**
      * 레벨의 절대 난이도를 반환합니다.
@@ -22,7 +22,7 @@ module.exports = {
      * @returns {Number} 레벨의 절대 난이도
      */
     getAbsoluteDifficulty: function(levelDiff, demonDiff, isAuto, isDemon){
-        var d;
+        let d;
         if(isAuto == "1") return this.AUTO;
         else if(isDemon == "1"){
             switch(demonDiff){

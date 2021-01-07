333daa61edaab3d256f35f0f2643229ccd8f4d67
DenFade#3350 <49348248+acceler8tion@users.noreply.github.com>
2021-01-07 13:29:01 +0900

    GDSong -> GDMusic words fixed

--- kakaoGDBot-api/GDMusic.js
+++ kakaoGDBot-api/GDMusic.js
@@ -20,27 +20,27 @@ function GDMusic(author, id, name, size, url, downloadURL) {
 GDMusic.EMPTY = new GDMusic("Unknown", 0, "Unknown");
 
 GDMusic.basicSongs = {
-    "0": new GDSong("ForeverBound", "Level 1", "Stereo Madness"),
-    "1": new GDSong("DJVI", "Level 2", "Back On Track"),
-    "2": new GDSong("Step", "Level 3", "Polargeist"),
-    "3": new GDSong("DJVI", "Level 4", "Dry Out"),
-    "4": new GDSong("DJVI", "Level 5", "Base After Base"),
-    "5": new GDSong("DJVI", "Level 6", "Cant Let Go"),
-    "6": new GDSong("Waterflame", "Level 7", "Jumper"),
-    "7": new GDSong("Waterflame", "Level 8", "Time Machine"),
-    "8": new GDSong("DJVI", "Level 9", "Cycles"),
-    "9": new GDSong("DJVI", "Level 10", "xStep"),
-    "10": new GDSong("Waterflame", "Level 11", "Clutterfunk"),
-    "11": new GDSong("DJ-Nate", "Level 12", "Theory of Everything"),
-    "12": new GDSong("Waterflame", "Level 13", "Electroman Adventures"),
-    "13": new GDSong("DJ-Nate", "Level 14", "Clubstep"),
-    "14": new GDSong("DJ-Nate", "Level 15", "Electrodynamix"),
-    "15": new GDSong("Waterflame", "Level 16", "Hexagon Force"),
-    "16": new GDSong("Waterflame", "Level 17", "Blast Processing"),
-    "17": new GDSong("DJ-Nate", "Level 18", "Theory of Everything 2"),
-    "18": new GDSong("Waterflame", "Level 19", "Geometrical Dominator"),
-    "19": new GDSong("F-777", "Level 20", "Deadlocked"),
-    "20": new GDSong("MDK", "Level 21", "Fingerdash")
+    "0": new GDMusic("ForeverBound", "Level 1", "Stereo Madness"),
+    "1": new GDMusic("DJVI", "Level 2", "Back On Track"),
+    "2": new GDMusic("Step", "Level 3", "Polargeist"),
+    "3": new GDMusic("DJVI", "Level 4", "Dry Out"),
+    "4": new GDMusic("DJVI", "Level 5", "Base After Base"),
+    "5": new GDMusic("DJVI", "Level 6", "Cant Let Go"),
+    "6": new GDMusic("Waterflame", "Level 7", "Jumper"),
+    "7": new GDMusic("Waterflame", "Level 8", "Time Machine"),
+    "8": new GDMusic("DJVI", "Level 9", "Cycles"),
+    "9": new GDMusic("DJVI", "Level 10", "xStep"),
+    "10": new GDMusic("Waterflame", "Level 11", "Clutterfunk"),
+    "11": new GDMusic("DJ-Nate", "Level 12", "Theory of Everything"),
+    "12": new GDMusic("Waterflame", "Level 13", "Electroman Adventures"),
+    "13": new GDMusic("DJ-Nate", "Level 14", "Clubstep"),
+    "14": new GDMusic("DJ-Nate", "Level 15", "Electrodynamix"),
+    "15": new GDMusic("Waterflame", "Level 16", "Hexagon Force"),
+    "16": new GDMusic("Waterflame", "Level 17", "Blast Processing"),
+    "17": new GDMusic("DJ-Nate", "Level 18", "Theory of Everything 2"),
+    "18": new GDMusic("Waterflame", "Level 19", "Geometrical Dominator"),
+    "19": new GDMusic("F-777", "Level 20", "Deadlocked"),
+    "20": new GDMusic("MDK", "Level 21", "Fingerdash")
 };
 
 module.exports = GDMusic;

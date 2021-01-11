829308ccd6e3681aaf52afc7301a5bb5020ef862
acceler8tion <rurchi1206@gmail.com>
2021-01-12 01:47:49 +0900

    fixed: toString method, JSDoc

--- kakaoGDBot-api/GDComment.js
+++ kakaoGDBot-api/GDComment.js
@@ -7,7 +7,7 @@
  * @param {Number} levelID 해당 코멘트가 올라온 레벨 ID
  * @param {Object} author 코멘트 저자
  * @param {Number} percent 프로그레스
- * @param {Number} role 코멘트 저자의 직업
+ * @param {String} role 코멘트 저자의 직업
  * @param {Object} iconInfo 코멘트 저자의 아이콘 정보
  */
 function GDComment(content, id, likes, date, levelID, author,
@@ -27,7 +27,25 @@ function GDComment(content, id, likes, date, levelID, author,
 }
 
 GDComment.prototype.toString = function() {
-    return JSON.stringify(this, null, 4);
+    return "content="+this.content+
+            "\nid="+this.id+
+            "\nlikes="+this.likes+
+            "\ndate="+this.date+
+            "\nlevelID="+this.levelID+
+            "\nauthor={"+
+            "\nname="+this.author.name+
+            "\nplayerID="+this.author.playerID+
+            "\naccountID="+this.author.accountID+
+            "\n}"+
+            "\npercent="+this.percent+
+            "\nrole="+this.role+
+            "\niconInfo={"+
+            "\nform="+this.iconInfo.form+
+            "\nicon="+this.iconInfo.icon+
+            "\ncol1="+this.iconInfo.col1+
+            "\ncol2="+this.iconInfo.col2+
+            "\nglow="+this.iconInfo.glow+
+            "\n}";
 }
 
 module.exports = GDComment;
